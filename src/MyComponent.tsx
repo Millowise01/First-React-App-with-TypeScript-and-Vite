b2d4e1f30d4dee49bab1f4abc8fa7820181ede18
Millowise01 <l.carew@alustudent.com>
2026-09-10 14:18:16 +0300

    first header and paragraph

---
+++ src/MyComponent.tsx
@@ -0,0 +1,12 @@
+    import React from 'react';
+
+    const MyComponent: React.FC = () => {
+      return (
+        <div>
+          <h1>Hello from MyComponent!</h1>
+          <p>This my first paragraph of text within my component.</p>
+        </div>
+      );
+    };
+
+    export default MyComponent;
