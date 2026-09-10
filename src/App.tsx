06de5c9beae41e48fe8c8787f4950de886773e0f
Millowise01 <l.carew@alustudent.com>
2026-09-10 14:52:55 +0300

    Fix code formatting by removing excessive leading indentation from source files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-    import MyComponent from './MyComponent';
-    import './App.css';
+import MyComponent from './MyComponent';
+import './App.css';
 
-    function App() {
-      return (
-        <div className="App">
-          <MyComponent />
-        </div>
-      );
-    }
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+    </div>
+  );
+}
 
-    export default App;
+export default App;
--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,13 +1,13 @@
-    import React from 'react';
-    import './MyComponent.css'; // Import the CSS file
+import React from 'react';
+import './MyComponent.css';
 
-    const MyComponent: React.FC = () => {
-      return (
-        <div className="my-component">
-          <h1>Hello from MyComponent!</h1>
-          <p>This is my paragraph of text within my component.</p>
-        </div>
-      );
-    };
+const MyComponent: React.FC = () => {
+  return (
+    <div className="my-component">
+      <h1>Hello from MyComponent!</h1>
+      <p>This is my paragraph of text within my component.</p>
+    </div>
+  );
+};
 
-    export default MyComponent;
+export default MyComponent;
